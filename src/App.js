37b76bdd90a3c9dc1273eda566c0bed51714116a
HEAL <HEAL@OOCL.COM>
2024-12-05 21:19:11 +0800

    [Parking Lot] choreinitialize project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import ParkingLot from "./component/ParkingLot";
+
+function App() {
+    return (
+        <div>
+            <ParkingLot/>
+        </div>
+
+    );
+}
+
+export default App;
